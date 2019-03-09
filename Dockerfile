a54d1984f94ecb282841d440d6851472110f18db
Brandon Elam Barker <beb82@cornell.edu>
2019-03-09 12:15:50 +0000

    converted to npm/parcel/bower build

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ MAINTAINER Brandon Elam Barker
 ENV PURESCRIPT_DOWNLOAD_SHA1 f0c9fae808ae27973de7c77519f87ae6e4837312
 ENV PSC_PACKAGE_DOWNLOAD_SHA1 09e033708f412e25ff049e5a9de42438b4696a33
 
-RUN npm install -g bower pulp@12.3.0
+RUN npm install -g bower parcel-bundler pulp@12.3.0
 
 RUN cd /opt \
     && wget https://github.com/purescript/purescript/releases/download/v0.12.3/linux64.tar.gz \
--- psc.sh
+++ psc.sh
@@ -3,8 +3,14 @@
 docker run --rm -ti \
        --volume /etc/passwd:/etc/passwd:ro \
        --volume "$PWD":/wd \
+       --volume "$HOME/.cache:$HOME/.cache" \
+       --volume "$HOME/.npmrc:$HOME/.npmrc" \
+       --volume "$HOME/.npm:$HOME/.npm" \
+       --volume "$HOME/.npm-packages:$HOME/.npm-packages" \
        --user "$UID" \
        --workdir /wd \
+       -e "XDG_CONFIG_HOME=/wd/.xdg_config_home" \
+       -e "XDG_DATA_HOME=/wd/.xdg_data_home" \
        purescript:0.12.3 "$@"
 
 # Add this before the last line (image name) for debugging:
