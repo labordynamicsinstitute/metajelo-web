76ad9a0c41d0830d6b00fd22d67b5ec530e09155
Brandon Barker <beb82@cornell.edu>
2019-03-12 20:38:33 +0000

    changing to button example

--- src/Main.purs
+++ src/Main.purs
@@ -1,15 +1,19 @@
 module Main where
 
-import Prelude
+import Prelude                              (Unit, discard, void, ($))
 
-import Concur.Core (Widget)
-import Concur.React (HTML)
-import Concur.React.DOM as D
-import Concur.React.Run (runWidgetInDom)
-import Effect (Effect)
+import Concur.Core                          (Widget)
+import Concur.React                         (HTML)
+import Concur.React.DOM                     (button, button', div', text)
+import Concur.React.Props                   (onClick)
+import Concur.React.Run                     (runWidgetInDom)
+import Effect                               (Effect)
 
-rootWidget :: forall a. Widget HTML a
-rootWidget = D.text "Hello World"
+hello :: forall a. Widget HTML a
+hello = do
+  -- void to discard the button click
+  void $ button [onClick] [text "Say Hello"]
+  text "Hello Sailor!"
 
 main :: Effect Unit
-main = runWidgetInDom "root" rootWidget
+main = runWidgetInDom "root" hello
