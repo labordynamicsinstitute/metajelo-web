597b1e8eae50bf17447fc0e4b53126f360e49fe3
Brandon Elam Barker <beb82@cornell.edu>
2019-03-13 20:30:36 +0000

    playing around with different ways of composing widgets

--- src/Main.purs
+++ src/Main.purs
@@ -1,19 +1,24 @@
 module Main where
 
-import Prelude                              (Unit, discard, void, ($))
+import Prelude
 
 import Concur.Core                          (Widget)
 import Concur.React                         (HTML)
 import Concur.React.DOM                     (button, button', div', text)
 import Concur.React.Props                   (onClick)
 import Concur.React.Run                     (runWidgetInDom)
+import Control.Alt                          ((<|>))
+import Control.MultiAlternative             (orr)
 import Effect                               (Effect)
 
 hello :: forall a. Widget HTML a
 hello = do
-  -- void to discard the button click
-  void $ button [onClick] [text "Say Hello"]
-  text "Hello Sailor!"
+  greeting <- orr
+    [ "Hello" <$ button [onClick] [text "Say Hello"]
+    , "Namaste" <$ button [onClick] [text "Say Namaste"]
+    ]
+
+  text (greeting <> " Sailor!")
 
 main :: Effect Unit
 main = runWidgetInDom "root" hello
