496f4fb58d705d125d5683340f5098af8809c5ad
Brandon Elam Barker <beb82@cornell.edu>
2019-03-15 19:21:35 +0000

    more example code added, still need Elm Form and signals working

--- src/Main.purs
+++ src/Main.purs
@@ -3,22 +3,38 @@ module Main where
 import Prelude
 
 import Concur.Core                          (Widget, andd)
+import Concur.Core.FRP                      (Signal, display, dyn, hold)
 import Concur.React                         (HTML)
-import Concur.React.DOM                     (button, button', div', input, p', text)
-import Concur.React.Props                   (onChange, onClick, onFocus, unsafeTargetValue)
+import Concur.React.DOM                     (button, button', div_, div',
+                                             input, p', text)
+import Concur.React.Props                   (_type, checked, onChange,
+                                             onChecked, onClick, onFocus,
+                                             unsafeTargetValue, value)
 import Concur.React.Run                     (runWidgetInDom)
 import Control.Alt                          ((<|>))
 import Control.MultiAlternative             (orr)
 import Data.Maybe                           (fromMaybe)
+import Data.Traversable                     (traverse)
 import Effect                               (Effect)
+import Effect.Class                         (liftEffect)
+import Effect.Class.Console                 (log)
 import React.SyntheticEvent                 (SyntheticMouseEvent)
 
 hello :: forall a. Widget HTML a
 hello = do
-  greeting <- orr
+  greeting :: String <- orr
     [ "Hello" <$ button [onClick] [text "Say Hello"]
     , "Namaste" <$ button [onClick] [text "Say Namaste"]
     ]
+  text (greeting <> " Sailor!")
+
+helloWidget :: forall a. Widget HTML a
+helloWidget = do
+  greeting :: String <- orr
+    [ "Hello" <$ button [onClick] [text "Say Hello"]
+    , "Namaste" <$ button [onClick] [text "Say Namaste"]
+    ]
+  liftEffect $ log ("You chose to say " <> greeting)
   text (greeting <> " Sailor!")
 
 buttonHello :: Widget HTML SyntheticMouseEvent
@@ -26,7 +42,8 @@ buttonHello = button [onClick] [text "Say Hello"]
 
 data Action = Changed String | Focused
 inputWidget1 :: Widget HTML Action
-inputWidget1 = input [(Changed <<< unsafeTargetValue) <$> onChange, Focused <$ onFocus]
+inputWidget1 = input [(Changed <<< unsafeTargetValue) <$> onChange,
+                      Focused <$ onFocus]
 
 type IWState = {focusCount:: Int, currentText :: String}
 inputWidget2 :: IWState -> Widget HTML IWState
@@ -50,7 +67,8 @@ focusCountWidget = go initState
       , inputWidget2 s
       ] >>= go
 
--- A counter widget takes the initial count as argument, and returns the updated count
+-- A counter widget takes the initial count as argument,
+-- and returns the updated count
 counter :: Int -> Widget HTML Int
 counter count = do
   void $ button [onClick] [text (show count)]
@@ -70,6 +88,77 @@ listCounters3 :: Array Int -> Widget HTML (Array Int)
 listCounters3 initialCounts = andd (map counter initialCounts)
 
 
+{- *** Not working currently
+-- This is like Elm's State
+type Form =
+  { name :: String
+  , rememberMe :: Boolean
+  }
+
+-- This is like Elm's Action
+data FormAction
+  = Name String
+  | RememberMe Boolean
+  | Submit
+
+formWidget :: Form -> Widget HTML Form
+formWidget form = do
+  -- This is like Elm's view function
+  res <- div'
+    [ Name <$> input [ _type "text", value form.name,
+                       unsafeTargetValue <$> onChange]
+    , RememberMe <$> input [_type "checkbox", checked form.rememberMe,
+                            onChecked]
+    , Submit <$ button [value "Submit", onClick]
+    ]
+  -- This is like Elm's update function
+  case res of
+    Name s -> formWidget (form {name = s})
+    RememberMe b -> formWidget (form {rememberMe = b})
+    Submit -> pure form
+-}
+
+
+
+getGreeting :: Widget HTML String
+getGreeting = div'
+    [ "Hello" <$ button [onClick] [text "Say Hello"]
+    , "Namaste" <$ button [onClick] [text "Say Namaste"]
+    ]
+
+showGreeting :: String -> Widget HTML Unit
+showGreeting greeting = div'
+  [ text (greeting <> " Sailor!")
+  , void $ button [onClick] [text "restart"]
+  ]
+
+helloGreets :: forall a. Widget HTML a
+helloGreets = do
+  greeting <- getGreeting
+  showGreeting greeting
+  helloGreets
+
+helloComp :: String -> forall a. Widget HTML a
+helloComp prev = helloComp =<< div'
+  [ text ("Previous greeting - " <> prev)
+  , do
+      greeting <- getGreeting
+      showGreeting greeting
+      pure greeting
+  ]
+
+{- FIXME: not working. understand type of display
+helloSig :: String -> Signal HTML String
+helloSig s = display $ helloComp s
+
+
+helloListWithDisplay :: Array String -> Signal HTML (Array String)
+helloListWithDisplay prev = div_ [] do
+  traverse helloComp prev
+  display (text ("Previously selected greetings - " <> show prev))
+-}
+
 main :: Effect Unit
-main = runWidgetInDom "root" $ focusCountWidget -- inputWidget2 $ {focusCount: 0, currentText: ""}
---main = runWidgetInDom "root" $ listCounters [1, 1, 2, 3, 5]
+main = runWidgetInDom "root" $ focusCountWidget
+-- main = runWidgetInDom "root" $ listCounters3 [1, 1, 2, 3, 5]
+-- main = runWidgetInDom "root" helloListWithDisplay
