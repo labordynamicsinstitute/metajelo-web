d95901311a4a53fd7dce370a3e17853cb4191869
Brandon Elam Barker <beb82@cornell.edu>
2019-04-19 21:23:03 +0000

    added browser test skeleton and test data

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,75 @@
 module Test.Main where
 
 import Prelude
-import Effect (Effect)
-import Effect.Console (log)
+
+import Data.Int                          (toNumber)
+import Data.Maybe                        (Maybe(..), fromMaybe)
+import Data.Natural                      (intToNat)
+-- import Debug.Trace                       (traceM)
+import Effect                            (Effect)
+import Effect.Aff                        (Aff)
+import Effect.Class                      (liftEffect)
+import Effect.Console                    (logShow)
+import Foreign                           (isUndefined, isNull, unsafeToForeign)
+import Test.Data                         as TD
+import Test.Unit                         (suite, test)
+import Test.Unit.Main                    (runTest)
+import Test.Unit.Assert                  as Assert
+
+import Web.DOM.Document                  (Document, toNode)
+import Web.DOM.DOMParser                 (DOMParser, makeDOMParser, parseXMLFromString)
+import Web.DOM.Document.XPath            (NSResolver)
+import Web.DOM.Document.XPath            as XP
+import Web.DOM.Document.XPath.ResultType as RT
+import Web.DOM.Element                   (Element, fromNode, getAttribute)
+import Web.DOM.Node                      (Node, nodeName)
+
+parseMetajeloDoc :: DOMParser -> Effect Document
+parseMetajeloDoc dp = parseXMLFromString TD.metajeloXml dp
+
+getMetajeloResolver :: Node -> Document -> Effect NSResolver
+getMetajeloResolver node doc = do
+  nsResolver <- XP.defaultNSResolver node doc
+  -- traceM nsResolver
+  nodeEleMay :: Maybe Element <- pure $ fromNode node
+  defaultNS :: String <- getDefaultNS nodeEleMay
+  pure $ XP.customNSResolver $ makeMjNSResFun nsResolver defaultNS
+  where
+    getDefaultNS :: Maybe Element -> Effect String
+    getDefaultNS mayElem = do
+      case mayElem of
+        Nothing -> pure $ guessedNS
+        Just elem -> map nsOrGuess (getAttribute "xmlns" elem)
+    guessedNS = "http://ourdomain.cornell.edu/reuse/v.01"
+    nsOrGuess :: Maybe String -> String
+    nsOrGuess nsMay = fromMaybe guessedNS nsMay
+    makeMjNSResFun :: NSResolver -> String -> String -> String
+    makeMjNSResFun nsr defNS prefix = case XP.lookupNamespaceURI nsr prefix of
+      Nothing -> defNS
+      Just ns -> ns
 
 main :: Effect Unit
-main = do
-  log "You should add some tests."
+main = runTest do
+  suite "namespaced tests" do
+    test "metajelo.xml" do
+      domParser <- liftEffect $ makeDOMParser
+
+      metajeloDoc <-liftEffect $ parseMetajeloDoc domParser
+      metajelo <- pure $ toNode metajeloDoc
+
+      mjNSresolver <- liftEffect $ getMetajeloResolver metajelo metajeloDoc
+
+      metajeloIdRes <- liftEffect $ XP.evaluate
+        "/foo:record/foo:identifier"
+        metajelo
+        (Just mjNSresolver)
+        RT.string_type
+        Nothing
+        metajeloDoc
+      metajeloId <- liftEffect $ XP.stringValue metajeloIdRes
+      tlog $ "got metajelo id" <> metajeloId
+      Assert.equal RT.string_type (XP.resultType metajeloIdRes)
+      Assert.equal "OjlTjf" metajeloId
+
+tlog :: forall a. Show a => a -> Aff Unit
+tlog = liftEffect <<< logShow
